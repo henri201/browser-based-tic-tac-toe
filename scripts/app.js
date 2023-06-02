561739cebca296389d56aaa092ca61a4f44bec27
hpriim2gi <henri.priim2gi@hotmail.com>
2023-06-02 18:15:03 +0300

    form update

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,17 @@
 // initalize the game
 const playerConfigOverlayElement = document.getElementById('config-overlay');
+const backDropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
 
 const editPlayer1ButtonElement = document.getElementById('edit-player-1-btn');
 const editPlayer2ButtonElement = document.getElementById('edit-player-2-btn');
+const cancelConfigBtn = document.getElementById('cancel-config-btn');
 
 
 editPlayer1ButtonElement.addEventListener('click', openPlayerConfig);
 editPlayer2ButtonElement.addEventListener('click', openPlayerConfig);
+
+cancelConfigBtn.addEventListener('click', closePlayerConfig);
+backDropElement.addEventListener('click', closePlayerConfig);
+
+formElement.addEventListener('submit', savePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,17 @@
 //take user input, validate, store
 
 function openPlayerConfig() {
-    
+    playerConfigOverlayElement.style.display = 'block'
+    backDropElement.style.display = 'block'
+}
+
+function closePlayerConfig() {
+    playerConfigOverlayElement.style.display = 'none'
+    backDropElement.style.display = 'none'
+}
+
+function savePlayerConfig (event) {
+    event.preventDefault();          //prevent the form from reloading the page
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get('playername');
 }
